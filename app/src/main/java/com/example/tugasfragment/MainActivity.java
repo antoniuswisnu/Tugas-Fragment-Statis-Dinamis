543575327d53c20f644081de5d3d408337c035c6
Antonius Wisnu <antonius.kri2003@mail.ugm.ac.id>
2023-02-24 20:29:11 +0700

    Add Fragment Static

--- app/src/main/java/com/example/tugasfragment/MainActivity.java
+++ app/src/main/java/com/example/tugasfragment/MainActivity.java
@@ -21,54 +21,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         mButton = findViewById(R.id.btn);
 
-        mButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (!isFragmentDisplayed) {
-                    displayFragment();
-                } else {
-                    closeFragment();
-                }
-            }
-        });
+
 
     }
 
-    public void displayFragment() {
-        SimpleFragment simpleFragment = SimpleFragment.newInstance();
 
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager
-                .beginTransaction();
-
-
-        // TODO: Add the SimpleFragment.
-        fragmentTransaction.replace(R.id.fragment_container,
-                simpleFragment).commit();
-
-        text = findViewById(R.id.tv1);
-        text.setText(R.string.lengkap);
-        isFragmentDisplayed = true;
-        mButton.setText("Mantap!");
-    }
-
-    public void closeFragment() {
-        // Get the FragmentManager.
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        // Check to see if the fragment is already showing.
-        SimpleFragment simpleFragment = (SimpleFragment) fragmentManager
-                .findFragmentById(R.id.fragment_container);
-        if (simpleFragment != null) {
-            // Create and commit the transaction to remove the fragment.
-            FragmentTransaction fragmentTransaction =
-                    fragmentManager.beginTransaction();
-            fragmentTransaction.replace(R.id.fragment_container,simpleFragment).commit();
-        }
-        // Update the Button text.
-        text = findViewById(R.id.tv1);
-        text.setText(R.string.pendek);
-        // Set boolean flag to indicate fragment is closed.
-        isFragmentDisplayed = false;
-        mButton.setText("Lihat Kelebihan");
-    }
 }
